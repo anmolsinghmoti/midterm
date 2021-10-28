c6ae33a3914086ed5afd7ea73b1fb5a2a855884a
Anmol Singh <anmolsinghmoti@gmail.com>
2021-10-28 10:12:12 -0400

    mongodb atlas added

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Anmol:YHB2g9DWfLy0kB33@mongodbserver.amhnd.mongodb.net/Book_Store?retryWrites=true&w=majority"
 };
